33ab35c58a11b9ecc7ec7668801f9337cf0219bb
Justin Rinehuls <jrinehuls@gmail.com>
2023-12-18 21:35:20 -0500

    Initial commit

---
+++ src/test/java/com/jrinehuls/employeemgmt/EmployeeMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrinehuls.employeemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
