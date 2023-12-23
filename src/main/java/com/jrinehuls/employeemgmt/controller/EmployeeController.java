8e67e005e7c4211b5111ca61850497157caddbc4
Justin Rinehuls <jrinehuls@gmail.com>
2023-12-23 15:48:02 -0500

    Add CORS policy

--- src/main/java/com/jrinehuls/employeemgmt/controller/EmployeeController.java
+++ src/main/java/com/jrinehuls/employeemgmt/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employee")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
 
     private EmployeeService employeeService;
